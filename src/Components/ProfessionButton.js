6200d0a07ecaa00c1e76142526eb5b9c1cea43de
Christopher DeVito <christopher.devito@protonmail.com>
2023-01-12 16:49:57 -0500

    Added EorzeaClock, ProfessionButton, and TImeController files

---
+++ src/Components/ProfessionButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfessionButton = ({ title }) => {
+    return (
+        <div>
+            <button>{title}</button>
+        </div>
+    )
+}
+
+export default ProfessionButton
---
+++ src/Controllers/TimeController.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+class TimeController {
+  const eorzeaMultiplier = 3600/175
+}
+
+export default TimeController
