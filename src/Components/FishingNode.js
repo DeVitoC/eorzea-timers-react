e6220c6bdc6bb5dbfc9a6ed5f01ca477d5dd85c1
Christopher DeVito <christopher.devito@protonmail.com>
2023-01-23 22:35:37 -0500

    Add node and fishing node.

---
+++ src/Components/FishingNode.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function FishingNode({ 
+  name, 
+  time, 
+  duration,
+  location, 
+  img, 
+  description, 
+  type, 
+  source, 
+  level, 
+  stars, 
+  x, 
+  y, 
+  expac, 
+  desynthLevel,
+  desynthJob,
+  mooch, 
+  moochFrom,
+  isWeatherChain,
+  weatherChain,
+  weather,
+  waterType,
+  gathering
+}) {
+
+}
---
+++ src/Components/Node.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Node({ 
+  name, 
+  time, 
+  location, 
+  img, 
+  description, 
+  type, 
+  source, 
+  level, 
+  stars, 
+  x, 
+  y, 
+  expac 
+}) {
+
+}
