46aa9af223c77d4b707a1858222e1c2983ea5fd8
Christopher DeVito <christopher.devito@protonmail.com>
2023-01-23 21:09:23 -0500

    Added SearchBar to NodeSelectionView

--- src/Components/NodeSelectionView.js
+++ src/Components/NodeSelectionView.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import EorzeaClock from "./EorzeaClock";
 import MobileHeader from "./Headers/MobileHeader";
+import SearchBar from "./SearchBar";
 import SortMenuMobile from "./SortMenuMobile";
 
 function NodeSelectionView({ profession }) {
+  const [searchField, setSearchField] = useState("")
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
+  }
+
   return (
     <div className="tc">
       <MobileHeader />
       <EorzeaClock labelType="p" />
       <SortMenuMobile />
+      <SearchBar searchChange={ onSearchChange } />
     </div>
   );
 }
