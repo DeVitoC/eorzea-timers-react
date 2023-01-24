de3b229b4e329731aa391a4d4093676e0206329f
Christopher DeVito <christopher.devito@protonmail.com>
2023-01-23 23:11:26 -0500

    Updated NodeList to return GatheringNodes

--- src/Components/NodeList.js
+++ src/Components/NodeList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import GatheringNode from "./GatheringNode";
 
 export default function NodeList({ nodes }) {
   return (
@@ -6,9 +7,22 @@ export default function NodeList({ nodes }) {
       {
         nodes.map((node, i) => {
           return (
-            <div>hello</div>
+            <GatheringNode 
+              name={node.name}
+              time={node.time}
+              location={node.location}
+              img={node.img}
+              description={node.description}
+              type={node.type}
+              source={node.source}
+              level={node.level}
+              stars={node.stars}
+              x={node.x}
+              y={node.y}
+              expac={node.expac}
+            />
           );
-        });
+        })
       }
     </div>
   );
